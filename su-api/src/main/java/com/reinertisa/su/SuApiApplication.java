f4331a3f89b1e2bb9926d1336b6879d355bec131
Isa Reinert <reinert.isa@gmail.com>
2024-11-24 16:06:04 -0800

    fix(api): create spring initialize project

---
+++ su-api/src/main/java/com/reinertisa/su/SuApiApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuApiApplication.class, args);
+    }
+
+}
---
+++ su-api/src/test/java/com/reinertisa/su/SuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.su;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
