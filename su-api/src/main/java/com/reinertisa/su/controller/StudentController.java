e5b92de6a016ea8b896e63ddf6f6e9eca1bd03a1
Isa Reinert <reinert.isa@gmail.com>
2024-11-25 23:31:35 -0800

    feat(api): create student rest controller

---
+++ su-api/src/main/java/com/reinertisa/su/controller/StudentController.java
@@ -0,0 +1,77 @@
+package com.reinertisa.su.controller;
+
+import com.reinertisa.su.exception.ResourceNotFoundException;
+import com.reinertisa.su.model.StudentDto;
+import com.reinertisa.su.model.StudentRequest;
+import com.reinertisa.su.service.StudentServiceImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    private final StudentServiceImpl studentServiceImpl;
+
+    @GetMapping(value = "")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<StudentDto>> getAllStudents() {
+        try {
+            return ResponseEntity.ok().body(studentServiceImpl.getAllStudents());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+    }
+
+    @GetMapping(value = "/{sid}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<StudentDto> getStudentById(@PathVariable(name = "sid") Long id) {
+        try {
+            return ResponseEntity.ok().body(studentServiceImpl.getStudentById(id));
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage(), ex);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+    }
+
+    @PostMapping(value = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createStudent(@RequestBody @Valid StudentRequest studentRequest) {
+        try {
+            studentServiceImpl.createStudent(studentRequest);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+    }
+
+    @PutMapping(value = "/{sid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateStudent(@PathVariable(name = "sid") Long id, @RequestBody @Valid StudentRequest studentRequest) {
+        try {
+            studentServiceImpl.updateStudent(id, studentRequest);
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage(), ex);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+    }
+
+    @DeleteMapping(value = "/{sid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteStudent(@PathVariable(value = "sid") Long id) {
+        try {
+            studentServiceImpl.deleteStudent(id);
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage(), ex);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+    }
+}
