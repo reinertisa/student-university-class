0c206de1baae605618bad3ad012de45ead2cfad7
Isa Reinert <reinert.isa@gmail.com>
2024-11-25 23:56:08 -0800

    feat(api): add many to many to student class

--- su-api/src/main/java/com/reinertisa/su/model/Student.java
+++ su-api/src/main/java/com/reinertisa/su/model/Student.java
@@ -3,6 +3,9 @@ package com.reinertisa.su.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter @Setter @Builder
 @AllArgsConstructor @NoArgsConstructor
 @Entity
@@ -18,5 +21,11 @@ public class Student {
 
     @Column(nullable = false)
     private Integer studentId;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinTable(name = "student_university_class",
+            joinColumns = {@JoinColumn(name = "student_id")},
+            inverseJoinColumns = {@JoinColumn(name = "university_class_id")})
+    private Set<UniversityClass> universityClasses = new HashSet<>();
 
 }
