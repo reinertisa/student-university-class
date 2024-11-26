7886f43b4ed6c07751a409af8ae592722b47baca
Isa Reinert <reinert.isa@gmail.com>
2024-11-25 22:43:38 -0800

    feat(api): create student repository

---
+++ su-api/src/main/java/com/reinertisa/su/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.reinertisa.su.repository;
+
+import com.reinertisa.su.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
