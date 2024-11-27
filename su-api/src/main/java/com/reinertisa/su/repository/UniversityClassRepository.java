cf0964b1008c2062987e6e098fe7baa82f59876e
Isa Reinert <reinert.isa@gmail.com>
2024-11-26 16:37:38 -0800

    feat(api): create university class repository

---
+++ su-api/src/main/java/com/reinertisa/su/repository/UniversityClassRepository.java
@@ -0,0 +1,7 @@
+package com.reinertisa.su.repository;
+
+import com.reinertisa.su.model.UniversityClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityClassRepository extends JpaRepository<UniversityClass, Long> {
+}
