ea3b6b0d860191100a331bfbedc77cbaba40ac36
Isa Reinert <reinert.isa@gmail.com>
2024-12-07 19:00:16 -0800

    fix(api): update ResourceNotFoundException

--- su-api/src/main/java/com/reinertisa/su/exception/ResourceNotFoundException.java
+++ su-api/src/main/java/com/reinertisa/su/exception/ResourceNotFoundException.java
@@ -1,11 +1,25 @@
 package com.reinertisa.su.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Getter
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception {
+
+    private String fieldName;
+
+    public ResourceNotFoundException() {
+        super();
+    }
+
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(String message, String fieldName) {
+        super(message);
+        this.fieldName = fieldName;
     }
 }
