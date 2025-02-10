1e475d2060d72c998d968a532b6115fa53a9a091
Isa Reinert <reinert.isa@gmail.com>
2025-02-09 20:02:16 -0800

    feat(website): create navbar component

---
+++ su-website/src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1>Student University Class Management</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New University Class</a>
+            </div>
+        </nav>
+    )
+}
