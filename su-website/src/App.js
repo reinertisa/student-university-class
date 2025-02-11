9ba02654d7b1f18df29ef5dc811fe2271bba4715
Isa Reinert <reinert.isa@gmail.com>
2025-02-10 18:04:17 -0800

    fix(website): add routes to app component

--- su-website/src/App.js
+++ su-website/src/App.js
@@ -1,16 +1,23 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import {BrowserRouter, Route, Routes} from "react-router";
+import StudentForm from "./pages/edit/Student/Form";
 
 function App() {
   return (
       <div className="App">
+          <BrowserRouter>
           <ThemeContextProvider>
               <Navbar />
               <div className="content">
-                  <Home />
+                  <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/createStudent" element={<StudentForm />} />
+                  </Routes>
               </div>
           </ThemeContextProvider>
+          </BrowserRouter>
       </div>
   );
 }
