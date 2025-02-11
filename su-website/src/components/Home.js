dd61dd6edca1283747543e6d07596b039a1c59f0
Isa Reinert <reinert.isa@gmail.com>
2025-02-10 18:04:31 -0800

    fix(website): update home component

--- su-website/src/components/Home.js
+++ su-website/src/components/Home.js
@@ -1,8 +1,10 @@
+import StudentPage from "../pages/Student";
 
 export default function Home() {
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <StudentPage />
         </div>
     );
 }
