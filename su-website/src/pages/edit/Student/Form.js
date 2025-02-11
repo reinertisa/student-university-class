f91f0ec398bede97149ff1a050954ac334c0e2c2
Isa Reinert <reinert.isa@gmail.com>
2025-02-10 18:03:51 -0800

    fix(website): add submit handle to student form component

--- su-website/src/pages/edit/Student/Form.js
+++ su-website/src/pages/edit/Student/Form.js
@@ -1,8 +1,10 @@
 import {FormProvider, useForm} from "react-hook-form";
 import FormText from "../../../components/Form/Text";
+import {useState} from "react";
 
 
 export default function StudentForm() {
+    const [error, setError] = useState('');
 
     const formMethods = useForm({
         defaultValues: {
@@ -13,13 +15,30 @@ export default function StudentForm() {
 
     const {handleSubmit} = formMethods;
 
-    const onSubmit = () => console.log("Data sumitted");
+    const onSubmit = async (values) => {
+        try {
+            const rez = await fetch('http://localhost:8080/api/v1/students', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: JSON.stringify(values),
+            })
+            const json = rez.json();
+            console.log(json);
+        } catch (err) {
+            setError(err);
+        }
+    };
 
     return (
         <FormProvider {...formMethods}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormText name="name" label="name" />
-                <FormText name="studentId" label="studentId" />
+            <form onSubmit={handleSubmit(onSubmit)} className="create-student">
+                <FormText name="studentName" label="Name" required={true} />
+                <FormText name="studentId" label="Id" required={true} />
+                <FormText name="studentEmail" label="email" required={true} />
+                <button>Save</button>
+                {error && <p>{error}</p>}
             </form>
         </FormProvider>
     )
